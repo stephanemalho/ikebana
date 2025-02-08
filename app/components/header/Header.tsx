6c32632a007ff499c48947e32d3064b22368b01a
AgipStephane <smalho@aexis.com>
2025-02-08 15:25:12 +0100

    ♻️ Header : extract strings to a separate folder

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,17 +1,18 @@
+import { stringHeader } from '@/app/constant/page/header';
 import React from 'react';
 
 const Header = () => {
     return (
         <header className="h-[90px] w-full flex flex-row">
             <section className="flex flex-col h-full justify-center items-center basis-2/5 bg-[#B1454A]">
-                <h1 className="secondary-title">Ikebana - Ohara</h1>
-                <p className="text-xs font-sans text-gray-300">Par Ikuko Kurenai KATO Sensei</p>
+                <h1 className="secondary-title">{stringHeader.title}</h1>
+                <p className="text-xs font-sans text-gray-300">{stringHeader.paragraph}</p>
             </section>
             <nav className="flex flex-row w-full basis-3/5">
                 <ul className="flex flex-row justify-evenly items-center w-1/2 ml-auto">
-                    <li>Découvrir</li>
-                    <li>Cours</li>
-                    <li>Contact</li>
+                    <li>{stringHeader.nav.discover}</li>
+                    <li>{stringHeader.nav.course}</li>
+                    <li>{stringHeader.nav.contact}</li>
                 </ul>
             </nav>
         </header>
---
+++ app/constant/page/header.ts
@@ -0,0 +1,9 @@
+export const stringHeader = {
+    title : "Ikebana - Ohara",
+    paragraph : "Par Ikuko Kurenai KATO Sensei",
+    nav : {
+        discover : "Découvrir",
+        course : "Nos Cours",
+        contact : "Contacter IKUKO"
+    }
+}
