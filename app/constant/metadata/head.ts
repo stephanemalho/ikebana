42739a6d3794b957798dd03a76e7959c08d5d258
AgipStephane <smalho@aexis.com>
2026-01-25 14:20:35 +0100

    ✨fix meta title

--- app/constant/metadata/head.ts
+++ app/constant/metadata/head.ts
@@ -1,5 +1,5 @@
 export const metaDataString = {
-    title: "École d'Ikebana Ohara - Cours et enseignement floral à Paris et dans les Yvelines",
+    title: " Ikebana école Ohara - chapitre Yvelines Paris, cours et enseignement de l'ikebana, art floral japonais à Paris et dans les Yvelines",
     description:
         "L'École d'Ikebana Ohara - Chapitre Yvelines Paris transmet l'art japonais de l'ikebana selon l'enseignement officiel de l'École Ohara, à travers trois lieux d'enseignement à Paris et en Île-de-France.",
     siteName: "Ikebana Ohara - Chapitre Yvelines",
